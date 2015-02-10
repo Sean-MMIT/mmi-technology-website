e6ddc3a1c7043b85cfadfaf8650d415bbfe56bfd
Sean Wright <sean@mmi.technology>
2015-02-09 19:12:59 -0700

    release one

--- hline.js
+++ hline.js
@@ -1,8 +1,16 @@
+var w = window.innerWidth;
 var canvas = document.getElementById('hline');
+canvas.width = w-(w*.1);
 var context = canvas.getContext('2d');
-context.beginPath();
-context.moveTo(0, 5);
-context.lineTo(1000, 5);
-context.linewidth = 2;
-context.strokeStyle = '#ffffff';
-context.stroke();
+
+function drawhline( canvas, context  )
+{
+	context.beginPath();
+	context.moveTo(0, 5);
+	context.lineTo(canvas.width, 5);
+	context.linewidth = 2;
+	context.strokeStyle = '#ffffff';
+	context.stroke();
+}
+
+drawhline( canvas, context );
